ceca3d15be5b9620a8b9aa57aa5ad1518f5258d1
bimiriya <poyarzunaspee@gmail.com>
2018-01-25 20:14:05 -0300

    avanzado

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-do { // do ejecutar función isValidCard if codigo no está vacía while código 
-  var codigo = prompt('Ingrese número de tarjeta');
-  if (codigo !== '') {
-    isValidCard();
+do {
+  var code = prompt('Enter credit card number');
+  if (code !== '' && code.length === 10) {
+    isValidCard(code);
+  } else {
+    alert('Please enter a valid card number');
   }
-} while (codigo === '' && typeof codigo !== 'number');
+} while (typeof code !== 'number' && code.length !== 10);
 
-function isValidCard() {
-  var emptyArr = [];
-  for (var i = 0 ; i < codigo.length ; i++) { // mete cada numbero singular en un nuevo array para convertirlos en elementos individuales
-    emptyArr.push(parseInt(codigo.charAt(i))); 
-    for (var j = 1 ; j < emptyArr.length ; j = j + 2) { // desde la posición 1 = segundo elemento mete los elementos pares en otro array
-      var emptyArr2 = [];
-      emptyArr2.push(parseInt(emptyArr.charAt(j)));
-      if (j * 2 >= 10) { // si los nuevos elementos en las posiciones pares al dividirlos por 2 >= 10
-        for (var x = 0 ; x < emptyArr2.length ; x++) {
-          for (var j = 1 ; j < emptyArr2.length ; j++) {
-            emptyArr2[j].charAt(x) + emptyArr2[j].charAt(j);
-          }
-        }
-      }
+
+function isValidCard(code) {
+  var nums = [];
+  var evens = [];
+  for (var i = 0; i < code.length ; i++) {
+    nums.push(parseInt(code[i]));
+    if (i % 2 !== 0) {
+      evens.push(nums.pop(nums[i])); 
     }
-  } // return
-};
+  }
+  var mult = evens.map(function(num) {
+    return num * 2;
+  });
+  console.log(mult);
+}
